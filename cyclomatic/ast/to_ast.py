ac129f2fc575ad87f719db0622071a6d511d99b3
furoxr <furuoxuan@wcoaltech.com>
2021-11-14 14:42:54 +0800

    feature:integrate with the tree-sitter

---
+++ cyclomatic/ast/to_ast.py
@@ -0,0 +1,52 @@
+import pathlib
+import cyclomatic
+from tree_sitter import (Language, Parser, Tree)
+
+
+package_path = pathlib.Path(cyclomatic.__file__).parent
+
+# currently supported parser
+# Key is the language label.
+# Value is the path to the tree-sitter grammar repo and the language name in tree-sitter
+_tree_sitter = {
+    'py': (package_path / 'ast' / 'tree-sitter-python', 'python')
+}
+
+SO_PATH = str(package_path / 'ast' / 'tree_sitter_binding.so')
+Language.build_library(SO_PATH, [str(s[0]) for s in _tree_sitter.values()])
+
+
+def to_ast(*, source: bytes = None, path: str = None, language=None) -> Tree:
+    """
+    convert source file or souce to a tree_sitter.Tree
+
+    :param source: souce code
+    :param path: source file path
+    :param language: supported language in
+    :return: tree_sitter.Tree
+    """
+    if path:
+        if language is None:
+            path = pathlib.Path(path)
+            suffix = path.suffix[1:]
+            language = suffix
+
+        if language not in _tree_sitter:
+            raise NotImplementedError(path.suffix[1:])
+
+        with open(path, 'rb') as f:
+            source = f.read()
+    elif source and language:
+        pass
+    else:
+        raise Exception(
+            "Wrong parameters! Try:\n"
+            "to_ast(path='path/to/source')\n"
+            "to_ast(path='path/to/source', language='py')\n"
+            "to_ast(source=b'def fun():\n    return 0')\n"
+        )
+
+    tree_sitter_lang = Language(SO_PATH, _tree_sitter[language][1])
+    parser = Parser()
+    parser.set_language(tree_sitter_lang)
+    return parser.parse(source)
