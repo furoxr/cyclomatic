a2b48318699a21a4aadb67bcd8693d78f39464ad
furoxr <furuoxuan@wcoaltech.com>
2021-11-14 20:43:11 +0800

    test:add tests

--- tests/test_py_calculator.py
+++ tests/test_py_calculator.py
@@ -53,10 +53,105 @@ def fun(a):
     c = PyCalculator()
     c.visit(tree.root_node)
 
-    print(c.block)
     # module cyclomatic complexity
     assert c.block.score == 3
 
     # function cyclomatic complexity
     assert c.block.sub_blocks[0].score == 2
 
+
+def test_for():
+    code = """
+def fun(a):
+    for i in range(100):
+        a += i
+    return a
+"""
+    tree, _ = to_ast(source=code.encode('utf-8'), language='py')
+    c = PyCalculator()
+    c.visit(tree.root_node)
+
+    # module cyclomatic complexity
+    assert c.block.score == 3
+
+    # function cyclomatic complexity
+    assert c.block.sub_blocks[0].score == 2
+
+
+def test_compose():
+    code = """
+def fun(a):
+    for i in range(100):
+        a += i
+    if a > 200:
+        a -= 40
+    else:
+        a = a * a
+    while a > 150:
+        a -= 1
+    return a
+"""
+    tree, _ = to_ast(source=code.encode('utf-8'), language='py')
+    c = PyCalculator()
+    c.visit(tree.root_node)
+
+    print(c.block)
+    # module cyclomatic complexity
+    assert c.block.score == 6
+
+    # function cyclomatic complexity
+    assert c.block.sub_blocks[0].score == 5
+
+
+def test_nested():
+    code = """
+def fun(a):
+    for i in range(100):
+        if i > 50:
+            a += 2
+        else:
+            a += 3
+        a += i
+    return a
+"""
+    tree, _ = to_ast(source=code.encode('utf-8'), language='py')
+    c = PyCalculator()
+    c.visit(tree.root_node)
+
+    # module cyclomatic complexity
+    assert c.block.score == 5
+
+    # function cyclomatic complexity
+    assert c.block.sub_blocks[0].score == 4
+
+
+def test_nested_function():
+    code = """
+def fun(a):
+    def wrap():
+        if a > 0:
+            return 1
+        else:
+            return 0
+ 
+    for i in range(100):
+        if i > 50:
+            a += 2
+        else:
+            a += 3
+        a += i
+    return a + wrap()
+    """
+    tree, _ = to_ast(source=code.encode('utf-8'), language='py')
+    c = PyCalculator()
+    c.visit(tree.root_node)
+
+    print(c.block)
+    # module cyclomatic complexity
+    assert c.block.score == 8
+
+    # function cyclomatic complexity
+    assert c.block.sub_blocks[0].score == 7
+
+    # nest function cyclomatic complexity
+    assert c.block.sub_blocks[0].sub_blocks[0].score == 3
