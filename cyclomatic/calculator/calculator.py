846bc267f5bb35b3e7e2e8e1ccef870c1b7311a4
furoxr <furuoxuan@wcoaltech.com>
2021-11-14 15:43:14 +0800

    fix:handle corner case

--- cyclomatic/calculator/calculator.py
+++ cyclomatic/calculator/calculator.py
@@ -1,12 +1,16 @@
 import tree_sitter
 from cyclomatic.calculator.base import Block
 from cyclomatic.config import LANGUAGE_MAPPING
+from typing import Optional
 
 
-def calculate(node: tree_sitter.Node, language: str) -> Block:
+def calculate(node: tree_sitter.Node, language: str) -> Optional[Block]:
     """
     calculate the cyclomatic complexity from the ast root node
     """
+    if node is None:
+        return None
+
     calculator_cls = LANGUAGE_MAPPING[language][-1]
     calculator = calculator_cls()
     calculator.visit(node)
